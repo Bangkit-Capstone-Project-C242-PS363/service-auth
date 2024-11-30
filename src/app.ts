35b8a144260565b2bb5c647817d03468d0d2ff15
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2024-11-30 21:42:24 +0700

    feat add posgresql

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,13 @@ import { AuthController } from "./infrastructure/http/controllers/auth.controlle
 import { createAuthRouter } from "./infrastructure/http/routes/auth.routes";
 import { JWTService } from "./domain/services/jwt.service";
 import { setupSwagger } from "./config/swagger";
+import { PostgresUserRepository } from "./infrastructure/repositories/posgresql-user.repository";
 
 const app = express();
 app.use(express.json());
 
-const userRepository = new InMemoryUserRepository();
+// const userRepository = new InMemoryUserRepository();
+const userRepository = new PostgresUserRepository();
 const jwtService = new JWTService();
 const authService = new AuthService(userRepository, jwtService);
 const authController = new AuthController(authService);
