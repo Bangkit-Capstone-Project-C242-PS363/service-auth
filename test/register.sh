fcec4a416e11c0c0585019a4dc3979625dd4ff61
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2024-11-30 19:06:41 +0700

    feat: add test

--- test/register.sh
+++ test/register.sh
@@ -1,1 +1,12 @@
-curl localhost:3000/auth/register -X POST -H "Content-Type: application/json" -d '{"username": "test", "email": "test@test.com", "password": "testaaa", "validationPassword": "testaaa"}'
+echo "Must be error if a field is missing"
+curl localhost:3000/auth/register \
+  -X POST \
+  -H "Content-Type: application/json " \
+  -d '{"username": "", "email": "", "password": "", "confirmPassword": ""}'
+echo ""
+
+echo "Must be success"
+curl localhost:3000/auth/register \
+  -X POST \
+  -H "Content-Type: application/json " \
+  -d '{"username": "test", "email": "test@test.com", "password": "123456789", "confirmPassword": "123456789"}'
