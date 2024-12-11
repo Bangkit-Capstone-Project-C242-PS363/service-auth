74eeafd196149d07e64a8ad12803249b0f979f2c
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2024-12-11 14:07:55 +0700

    cicd: returns domain after deploy

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,5 @@ podman build -t signmaster-auth .
 podman tag signmaster-auth asia-southeast2-docker.pkg.dev/capstone-project-c242-ps363/backend/signmaster-auth
 podman push asia-southeast2-docker.pkg.dev/capstone-project-c242-ps363/backend/signmaster-auth
 gcloud run deploy signmaster-auth --image=asia-southeast2-docker.pkg.dev/capstone-project-c242-ps363/backend/signmaster-auth --platform=managed --region=asia-southeast2 --allow-unauthenticated
+
+gcloud run services describe signmaster-auth --platform=managed --region=asia-southeast2 --format='value(status.url)'
